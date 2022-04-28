78e82e0de9cf160503b0a85b72d45aef00f05b16
NidhalNouma <nidhal.nouma.0@gmail.com>
2022-04-28 23:49:58 +0100

    adding model for custom date

--- src/components/photosViews/screen2/index.tsx
+++ src/components/photosViews/screen2/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IonDatetime } from "@ionic/react";
+import { IonModal } from "@ionic/react";
 
 import "./index.css";
 
@@ -16,7 +17,31 @@ const Screen2: React.FC<sProps> = ({ setDate, back }) => {
   ];
 
   const [custom, setCustom] = useState(false);
+  const [done, setDone] = useState(false);
   const [cdate, setCDate] = useState<string>("");
+
+  useEffect(() => {
+    let interval: any = null;
+    if (done && cdate && custom) {
+      // console.log("done");
+      setCustom(false);
+    } else if (done && cdate && !custom) {
+      console.log("done 11");
+      // setCustom(false);
+      // setDate(cdate ? new Date(cdate) : new Date());
+      // setDate((v: any) => {
+      //   if (done) return cdate ? new Date(cdate) : new Date();
+      //   return null;
+      // });
+      interval = setInterval(() => {
+        setDate(cdate ? new Date(cdate) : new Date());
+      }, 200);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [done, custom]);
 
   return (
     <React.Fragment>
@@ -24,48 +49,53 @@ const Screen2: React.FC<sProps> = ({ setDate, back }) => {
         <h3>Set appear date.</h3>
       </div>
       <div className="div-date">
-        {!custom ? (
-          <React.Fragment>
-            {period.map((v, i) => (
-              <button key={i} className="btn" onClick={() => setDate(v.v)}>
-                {v.name}
-              </button>
-            ))}
-            <button className="btn" onClick={() => setCustom(true)}>
-              Custom
+        <React.Fragment>
+          {period.map((v, i) => (
+            <button key={i} className="btn" onClick={() => setDate(v.v)}>
+              {v.name}
             </button>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
+          ))}
+          <button className="btn" onClick={() => setCustom(true)}>
+            Custom
+          </button>
+        </React.Fragment>
+        <button
+          className="btn"
+          onClick={() => (custom ? setCustom(false) : back())}
+        >
+          Back
+        </button>
+      </div>
+      <IonModal
+        className="modal"
+        mode={undefined}
+        animated={false}
+        isOpen={custom}
+        onDidDismiss={() => setCustom(false)}
+      >
+        <React.Fragment>
+          <div className="model-date">
             <IonDatetime
               className="date-picker"
               onIonChange={(e: any) => setCDate(e.detail.value!)}
             ></IonDatetime>
             <div className="mid-btn">
-              <button
-                className="btn"
-                onClick={() => (custom ? setCustom(false) : back())}
-              >
+              <button className="btn" onClick={() => setCustom(false)}>
                 Back
               </button>
               <button
                 className="btn"
-                onClick={() => setDate(cdate ? new Date(cdate) : new Date())}
+                onClick={() => {
+                  setDone(true);
+                  setCustom(false);
+                }}
               >
                 Next
               </button>
             </div>
-          </React.Fragment>
-        )}
-        {!custom && (
-          <button
-            className="btn"
-            onClick={() => (custom ? setCustom(false) : back())}
-          >
-            Back
-          </button>
-        )}
-      </div>
+          </div>
+        </React.Fragment>
+      </IonModal>
     </React.Fragment>
   );
 };
